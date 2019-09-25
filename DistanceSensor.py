ab0e3684af2b148e60ec61eaefeac8e64d4629f0
Philip G Breen <pbreen40@ccs.local>
2019-09-25 11:28:29 -0400

    Added the classes assignment

--- DistanceSensor.py
+++ DistanceSensor.py
@@ -1,29 +1,45 @@
-import time
 import board
-import adafruit_hcsr04
-import math
-import simpleio
 import neopixel
+#import math
+import time
+#import digitalio
+#import adafruit_bus_device
+import adafruit_hcsr04
+import simpleio
 
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D7, echo_pin=board.D2)
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D6, echo_pin=board.D5)
+#simpleio.map_range(x, in_min, in_max, out_min, out_max)
+
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness = .1)
-r = 0
-b = 0
-g = 0
-while True:
-    try:
-        if sonar.distance <= 20:
-            r = simpleio.map_range(sonar.distance, 0, 20, 255, 0)
-            b = simpleio.map_range(sonar.distance, 5, 20, 0, 255)
-            g = simpleio.map_range(sonar.distance, 20, 35, 0, 255)
-        else:
-            r = simpleio.map_range(sonar.distance, 0, 20, 255, 0)
-            b = simpleio.map_range(sonar.distance, 20, 35, 255, 0)
-            g = simpleio.map_range(sonar.distance, 20, 35, 0, 255)
-        #time.sleep(0.1)
-        dot.fill((int(r),int(g),int(b)))
 
-            #print((sonar.distance,))
-            #mapped_value = simpleio.map_range(sonar.distance, 0, 100, 0, 10)
+
+
+r = 0
+g = 0
+b = 0
+
+
+while True:
+
+    try:
+        print((sonar.distance,))
+        if sonar.distance <= 20:
+
+            r = simpleio.map_range(sonar.distance, 0,20,255,0)
+            b = simpleio.map_range(sonar.distance, 5,20,0,255)
+            g = simpleio.map_range(sonar.distance, 20,35,0,255)
+
+        else:
+            r = simpleio.map_range(sonar.distance, 0,20,255,0)
+            b = simpleio.map_range(sonar.distance, 35,20,0,255)
+            g = simpleio.map_range(sonar.distance, 20,35,0,255)
+
+        dot.fill((int(r),int(g),int(b)))
     except RuntimeError:
-        time.sleep(0.1)
+        print("Error")
+
+
+
+
+
+    time.sleep(0.1)
